620ccb4794e076d03c8fc94d5ea84cdeb7b1ee29
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2025-03-12 18:41:20 -0700

    Added keeping track of time; fixed clear();

--- Queue/SimulationEventQueue.java
+++ Queue/SimulationEventQueue.java
@@ -15,14 +15,14 @@ import java.util.*;
  */
 
 public class SimulationEventQueue implements SimulationEventQueueInterface{
-    EventNode endNode = null;
     EventNode startNode = null;
     int numNodes = 0;
-    
+    private double lastTime = 0.0;
+
     private class EventNode{
         SimulationEvent data;
         EventNode next;
-        
+
         EventNode(){
             data = null;
             next = null;
@@ -36,72 +36,84 @@ public class SimulationEventQueue implements SimulationEventQueueInterface{
 
     /** Adds a new event to this event queue.  If the time of the event to be added
      * is earlier the the time for this event queue, do not add the event.
-     * @param newEntry An event. 
+     * @param newEntry An event.
         */
 	public void add(SimulationEvent newEntry){
 	    //version one: add to end
-	    EventNode added = new EventNode(newEntry);
-	    added.next = null;
-	    endNode.next = added;
-	    endNode = added;
-	    numNodes++;
+		if(isEmpty()) {
+			startNode = new EventNode(newEntry);
+			numNodes = 1;
+			lastTime = startNode.data.getTime();
+		}else{
+			EventNode added = new EventNode(newEntry);
+			EventNode e = startNode;
+			while(e.next != null){e = e.next;}
+			e.next = added;
+			numNodes++;
+		}
     }
-	
+
 	/** Removes and returns the item with the earliest time.
 	 * @return The event with the earliest time or,
-	 * if the event queue was empty before the operation, null. 
+	 * if the event queue was empty before the operation, null.
          */
 	public SimulationEvent remove(){
-	    EventNode ret = startNode;
-	    startNode = startNode.next;
-        numNodes--;
-	    return ret.data;
+		if(isEmpty()){
+			return null;
+		}
+		else {
+			EventNode ret = startNode;
+			lastTime = ret.data.getTime();
+			startNode = startNode.next;
+			numNodes--;
+			return ret.data;
+		}
     }
-	
+
 	/** Retrieves the item with the earliest time.
-	 * @return The event with the earliest time or, 
-	 * if the event queue was empty was empty before the operation, null. 
+	 * @return The event with the earliest time or,
+	 * if the event queue was empty was empty before the operation, null.
          */
 	public SimulationEvent peek(){
 	    return startNode.data;
     }
-	
+
 	/** Detects whether this event queue is empty.
-	 * @return True if the event queue is empty. 
+	 * @return True if the event queue is empty.
          */
 	public boolean isEmpty(){
 	    return(startNode == null);
     }
-	
+
 	/** Gets the size of this event queue.
-	 * @return The number of entries currently in the event queue. 
+	 * @return The number of entries currently in the event queue.
          */
 	public int getSize(){
 	    return numNodes;
     }
-	
-	/** Removes all entries from this event queue. 
+
+	/** Removes all entries from this event queue.
          */
 	public void clear(){
-	    EventNode e = startNode;
-	    while(e != null){
-	        EventNode n = e.next;
-	        e = null;
-	        e = n;
+	    while(startNode != null){
+	        EventNode n = startNode.next;
+	        startNode = null;
+	        startNode = n;
+	        numNodes--;
         }
     }
-	
-	
+
+
 	/**
 	 * The current time of the simulation
-	 * 
+	 *
 	 * @return The time for the first event on the queue.
 	 */
 	public double getCurrentTime(){
-	    return startNode.data.getTime();
+	    return lastTime;
     }
-    
-    
+
+
 }
 
     
